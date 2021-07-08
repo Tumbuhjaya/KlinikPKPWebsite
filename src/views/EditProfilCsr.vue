61bec302c318556af6ee388773b28d6af6fbbdb2
tatawedha <tatawedha@gmail.com>
2021-07-08 14:18:50 +0700

    fix

--- src/views/EditProfilCsr.vue
+++ src/views/EditProfilCsr.vue
@@ -66,7 +66,7 @@
             </b-form-group>
             <b-form-group class="m-t-15" >
                 <div style="width:150px;height:150px;" v-if="datasCsr.srcLogo != ipBackEnd+'null'">
-                  <img :src="datasCsr.src" alt="" style="width:150px;height:150px">
+                  <img :src="datasCsr.srcLogo" alt="" style="width:150px;height:150px">
                 </div>
                 
                 <div style="width:150px;height:150px;" v-if="datasCsr.srcLogo == ipBackEnd+'null'">
