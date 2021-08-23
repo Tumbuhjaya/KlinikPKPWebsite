3972da1f8b4d7190705861d452d884f006c9b74e
tatawedha <tatawedha@gmail.com>
2021-08-23 16:00:00 +0700

    merge

--- src/views/DetailPerumahan.vue
+++ src/views/DetailPerumahan.vue
@@ -81,7 +81,13 @@
               </b-col>
               <b-col md="12">
                 <iframe
-                  :src="'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d' + dataPerum.koordinatY + '!2d110.38170531414542!3d' + dataPerum.koordinatX + '!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNsKwNTknMDUuMCJTIDExMMKwMjMnMDIuMCJF!5e0!3m2!1sid!2sid!4v1624201365485!5m2!1sid!2sid'"
+                  :src="
+                    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d' +
+                      dataPerum.koordinatY +
+                      '!2d110.38170531414542!3d' +
+                      dataPerum.koordinatX +
+                      '!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNsKwNTknMDUuMCJTIDExMMKwMjMnMDIuMCJF!5e0!3m2!1sid!2sid!4v1624201365485!5m2!1sid!2sid'
+                  "
                   width="100%"
                   height="300"
                   style="border: 0"
