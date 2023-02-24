776398335ff6a1960beda536f0728054b4660ddb
DANANG SR <danangsukoraharjo@gmail.com>
2023-02-24 13:50:50 +0700

    pull server

--- src/ipBackEnd.js
+++ src/ipBackEnd.js
@@ -1,3 +1,4 @@
-const backEnd = "http://localhost:8810/";
+// const backEnd = "http://localhost:8810/";
+const backEnd = "https://klinikpkp.disperakim.jatengprov.go.id/api/";
 
 module.exports = backEnd;
